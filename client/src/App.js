2d90fef7517b530018e47039603b92a285bb9f7c
DannyDoneva96 <danielasinmas1@gmail.com>
2023-03-10 07:50:16 +0200

    create hader component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './Reset.css';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 function App() {
   return (
     <div className="App">
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,20 +4,13 @@ const Header = () => {
     const user = true;
   return (
     <div className='header-container'>
-        <img src={require('../assets/images/Task Manager.png')} alt="" />
+        <img src={require('../../assets/images/Task Manager.png')} alt="" />
        
-        {user?
          <ul className='header-options'> 
             <li>Tasks</li>
             <li>Employees</li>
-            <li>Logout</li>
          </ul>
-        : <ul className='header-options'>
-            <li>Login</li>
-            <li>Tasks</li> 
-            <li>Employees</li>
-
-         </ul>}
+        
 
 
     </div>
