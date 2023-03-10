6bbc1697fd9df7be8fd68a3b5d94db9598325fd7
DannyDoneva96 <danielasinmas1@gmail.com>
2023-03-10 10:58:15 +0200

    add React router dom
    implement the browser router
    created employee components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
+
+import { Route, Routes  } from 'react-router-dom'
+
 import './Reset.css';
 import './App.css';
 
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-
+import Employees from './components/Employees/Employees';
 function App() {
   return (
-    <div className="App">
+    <div className="App">  
      <Header/> 
-     <Home/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/employees" element={<Employees />} />
+          
+          {/* <Route path="/loading" element={<Loading />} /> */}
+
+       </Routes>
+  
     </div>
   );
 }
