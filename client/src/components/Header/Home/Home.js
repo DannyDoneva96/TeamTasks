7f3b2f0faa01084c07dc68723e1717c963db9f06
DannyDoneva96 <danielasinmas1@gmail.com>
2023-03-10 08:06:45 +0200

    Update Home.js

--- client/src/components/Header/Home/Home.js
+++ client/src/components/Header/Home/Home.js
@@ -1,13 +1,26 @@
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div className="home-container">
+    return (
+        <div className="home-container">
 
-        
-      
-    </div>
-  )
+            <div className="home-get-started">
+                <div className="home-col-1">
+                    <h1 className="main-h">What is task manager?</h1>
+                    <p className="main-p">A task manager is an application that helps our company organize our work and stay focused. It helps us stay on top of our work, tackle multiple tasks, and stay organized. You can use a task manager to keep your work organized day in and day out. </p>
+                </div>
+                <div className="home-col-2">
+                    <h2 className="home-h2">What are task manager apps used for?</h2>
+                    <p className="home-p">Task manager apps are used by individuals or small and larger teams to efficiently accomplish tasks and complete projects by organizing and prioritizing their work. </p>
+                </div>
+
+            </div>
+            <div className="home-topEmployee">
+                <img src={require('')} alt="" />
+            </div>
+
+        </div>
+    )
 }
 
 export default Home
