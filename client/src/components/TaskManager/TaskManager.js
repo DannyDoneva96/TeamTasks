fcff8b7673860adc28797fb605a69882b517f082
DannyDoneva96 <danielasinmas1@gmail.com>
2023-03-11 16:32:49 +0200

    delete functionality

--- client/src/components/TaskManager/TaskManager.js
+++ client/src/components/TaskManager/TaskManager.js
@@ -1,10 +1,11 @@
-import React,{useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import './TaskManager.css'
 import Tasks from './Tasks/Tasks'
 import TaskModal from './Modal/TaskModal'
 import { DragDropContext } from "react-beautiful-dnd";
 import { db } from '../../firebase'
 import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc } from 'firebase/firestore'
+import { nanoid } from '@reduxjs/toolkit';
 
 const TaskManager = () => {
 
@@ -12,10 +13,33 @@ const TaskManager = () => {
 
     const [show, setShow] = useState(false)
     const [tasks, setTasks] = useState([])
+    const [todos, setTodos] = useState([])
+    useEffect(() => {
+        const getTasks = async () => {
+            const data = await getDocs(taskRef);
+            const tasks = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+            setTasks(tasks);
+            setTodos(tasks.filter((task) =>
+                task.status === 'To do'))
+
+        }
+       
+        getTasks()
+
+    }, []);
 
     const addTask = async (task) => {
         await addDoc(taskRef, task)
         setTasks([...tasks, task]);
+    }
+
+    const deleteTask = async (id) => {
+        const taskDoc = doc(db, "tasksManager", id);
+        await deleteDoc(taskDoc);
+        // remove from the state
+        
+        setTodos(prevState => prevState.filter(emp => emp.id !== id));
+           
     }
     return (
         <div>
@@ -24,36 +48,37 @@ const TaskManager = () => {
                     className="INPUT-TASKmANAGER-SEARCH"
                     placeholder='Search Tasks'
                 />
-                <button onClick={() => setShow(true)}  className="newTaskAdd">+ Add Task</button>
-                <TaskModal onClose={() => setShow(false)} show={show} addTask={addTask}/>
+                <button onClick={() => setShow(true)} className="newTaskAdd">+ Add Task</button>
+                <TaskModal onClose={() => setShow(false)} show={show} addTask={addTask} />
             </div>
 
             <div className="container-task-manager">
                 <div className="column-tasks">
                     <h3>To Do</h3>
                     <div className="to-do">
-                        <Tasks/>
-                        <Tasks/>
-                        <Tasks/>
-                        <Tasks/>
-                        </div>
+                    {todos
+                        ? todos.map((task) => {
+                            return (
+                                
+                                    <Tasks key={nanoid()} deleteTask={deleteTask} task={task} />
+                               
+                            );
+                        }) : <p>No Tasks found.</p>}
+                    </div>
                 </div>
                 <div className="column-tasks">
-                <h3>In Progress</h3>
-                <div className="to-do"></div>
-                <Tasks/>
+                    <h3>In Progress</h3>
+                    <div className="to-do"></div>
                 </div>
-                
+
                 <div className="column-tasks">
-                <h3>In Review</h3>
-                <div className="to-do"></div>
-                <Tasks/>
-                <Tasks/>
+                    <h3>In Review</h3>
+                    <div className="to-do"></div>
+                   
                 </div>
                 <div className="column-tasks">
-                <h3>Done</h3>
-                <div className="to-do"></div>
-                <Tasks/>
+                    <h3>Done</h3>
+                    <div className="to-do"></div>
                 </div>
             </div>
         </div>
--- client/src/components/TaskManager/Tasks/Tasks.js
+++ client/src/components/TaskManager/Tasks/Tasks.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import './Task.css'
 
-const Tasks = () => {
+const Tasks = (props) => {
+
     return (
         <div className="to-do-cont">
             <div className="cont-task-s">
-                <h4 className="title-task">Task 1 </h4>
+                <h4 className="title-task">{props.task.title}</h4>
                 <div className="icons-task">
                     <i className="fa-regular fa-pen-to-square"></i>
-                    <i className="fa-solid fa-trash"></i>
+                   <button className='delete-task-btn' onClick={()=>props.deleteTask(props.task.id)}> <i className="fa-solid fa-trash"></i></button>
                 </div>
                 
-            </div><div className="descript-task">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est voluptate id</div>
+            </div><div className="descript-task">{props.task.description}</div>
             <div className="second-part">
-                <div className="timeSet"><i className="fa-regular fa-clock"></i>To be completed before:</div>
+                <div className="timeSet"><i className="fa-regular fa-clock"></i>To be completed before:{props.task.dueDate}</div>
                 <button className="status">Completed</button>
             </div>
         </div>
