2382d91aaaa4c676b64708fce704e59e11d1ab31
DannyDoneva96 <danielasinmas1@gmail.com>
2023-03-11 12:11:47 +0200

    installed new dependancy
    React beautiful drag and drop

--- client/src/components/TaskManager/TaskManager.js
+++ client/src/components/TaskManager/TaskManager.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import './TaskManager.css'
+import { DragDropContext } from "react-beautiful-dnd";
 
 const TaskManager = () => {
   return (
     <div>
-      
+      <div className="search-field-taskM">
+      <input
+        className="INPUT-TASKmANAGER-SEARCH"
+        placeholder='Search Tasks'
+      />
+      <button className="newTaskAdd">+ Add Task</button>
+      </div>
+
+      <div className="container-task-manager">
+
+      </div>
     </div>
   )
 }
